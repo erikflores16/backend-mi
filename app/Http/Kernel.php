86e47fb6fba7d001846a0c04ef4a860b7456ee6b
erik flores <erikfloresherrera290@gmail.com>
2025-07-31 23:29:35 +0000

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,11 +38,13 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-        'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+     'api' => [
+    \Illuminate\Http\Middleware\HandleCors::class,
+    // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+],
+
     ];
 
     /**
