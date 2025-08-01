170174034bba1ee8b5ddf3785a25cfa990d699d8
erik flores <erikfloresherrera290@gmail.com>
2025-08-01 09:50:34 +0000

    facades

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth; // ← esta línea es clave
+
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
