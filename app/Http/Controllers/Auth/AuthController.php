f4ff465077aa51c8c68de9eaee6d0141c1bfb265
erik flores <erikfloresherrera290@gmail.com>
2025-08-01 09:46:22 +0000

    login rquest

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\Auth\LoginRequest;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
